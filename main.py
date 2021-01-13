a2225ad20c5360cc28f30a22ca9ff141f276861f
tush-tr <iamtush-tr@DESKTOP-T3039R4.localdomain>
2021-01-14 00:24:06 +0530

    main script

--- main.py
+++ main.py
@@ -2,27 +2,27 @@ from flask import Flask, jsonify
 from flask import abort
 from flask import make_response
 from flask import request
+from data import data
+from flask_httpauth import HTTPBasicAuth
+auth = HTTPBasicAuth()
 app = Flask(__name__)
-data = [
-    {
-        'id':1,
-        'name': 'Rocky',
-        'breed': 'germen shephered',
-        'age': 2
-    },
-    {
-        'id':2,
-        'name': 'Tommy',
-        'breed': 'Labra',
-        'age': 1
-    }
-]
+
+# TODO: Authentication
+@auth.get_password
+def get_password(username):
+    if username == 'tushar':
+        return 'mycode'
+    return None
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+# $ curl -u tushar:mycode -i http://localhost:5000
 
 # TODO: Get Request stuff (showing data)
-
-@app.route('/',methods =['GET'])
+@app.route('/', methods=['GET'])
+@auth.login_required
 def show():
-    return jsonify({'data':data})
+    return jsonify({'data': data})
 # curl -i http://localhost:5000
 
 # For getting data of a specific id
@@ -32,6 +32,7 @@ def showwithid(data_id):
     if len(showdata) == 0:
         abort(404)
     return jsonify({'task': showdata[0]})
+
 # curl -i http://localhost:5000/id-number
 
 
@@ -50,7 +51,7 @@ def create_data():
     added = {
         'id': data[-1]['id'] + 1,
         'name': request.json['name'],
-        'breed': request.json.get('breed'," "),
+        'breed': request.json.get('breed', " "),
         'age': request.json.get('age')
     }
     # Append to main data list
@@ -58,6 +59,35 @@ def create_data():
     return jsonify({'data': added}), 201
 # curl -i -H "Content-Type: application/json" -X POST -d '{"name":"Jabb","breed":"labra","age":3}' http://localhost:5000
 
+# TODO: PUT method
+@app.route('/<int:data_id>', methods=['PUT'])
+def update_data(data_id):
+    updata = [updata for updata in data if updata['id'] == data_id]
+    if len(updata) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'name' in request.json and type(request.json['name']) != str:
+        abort(400)
+    if 'breed' in request.json and type(request.json['breed']) is not str:
+        abort(400)
+    if 'age' in request.json and type(request.json['age']) is not int:
+        abort(400)
+    updata[0]['name'] = request.json.get('name', updata[0]['name'])
+    updata[0]['breed'] = request.json.get('breed', updata[0]['breed'])
+    updata[0]['age'] = request.json.get('age', updata[0]['age'])
+    return jsonify({'Updated Data': updata[0]})
+# curl -i -H "Content-Type: application/json" -X PUT -d '{"name":"Jack","breed":"pug","age":3}' http://localhost:5000/2
+
+
+# TODO: DELETE Method
+@app.route('/<int:data_id>', methods=['DELETE'])
+def delete_task(task_id):
+    deldata = [deldata for deldata in data if deldata['id'] == task_id]
+    if len(deldata) == 0:
+        abort(404)
+    data.remove(deldata[0])
+    return jsonify({'result': True})
 
 
 
